8bd0a96fbabdfca4af49151a901bb5e1adeff888
Che <vee.langat@gmail.com>
2021-03-02 21:12:31 +0400

    initial commit

---
+++ AddView.swift
@@ -0,0 +1,57 @@
+//
+//   AddView.swift
+//  iExpense
+//
+//  Created by Chepkorir Lang'at on 01/03/2021.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    static let types = ["Business", "Personal"]
+    
+    @State private var showingAlert = false
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self){
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing:
+                                    Button("Save"){
+                                        if let actualAmount = Int(self.amount){
+                                            let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                                            self.expenses.items.append(item)
+                                            self.presentationMode.wrappedValue.dismiss()
+                                        } else {
+                                            showingAlert = true
+                                        }
+                                    })
+            .alert(isPresented: $showingAlert){
+                Alert(title: Text("Oops"), message: Text("Please enter a number"), dismissButton: .default(Text("Got it!")))
+            }
+        }
+    }
+    
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chepkorir Lang'at on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
